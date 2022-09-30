6204cb40a750b05ce82dcac0525a4a2741b1690c
NomanKhan3701 <noman.khan3701@gmail.com>
2022-09-30 12:47:19 +0530

    Removed env and added models

--- models/post.js
+++ models/post.js
@@ -1,37 +1,21 @@
 const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  desc: {
-    type: String,
-    required: true,
-  },
-  tags: {
-    type: [String],
-    required: true,
-  },
-  createdBy: {
-    type: String,
-    required: true,
-  },
-  img: {
-    type: String,
-  },
-  like: {
-    type: [String],
-    default: [],
-  },
-  comment: {
-    type: [String],
-    default: [],
-  },
-  createdAt: {
-    type: Date,
-    default: new Date(),
-  },
+	textContent: {
+		type: String,
+		required: true,
+	},
+	createdBy: {
+		type: String,
+		required: true,
+	},
+	img: {
+		type: String,
+	},
+	createdAt: {
+		type: Date,
+		default: new Date(),
+	},
 });
 
 module.exports = mongoose.models.Post || mongoose.model("Post", postSchema);
--- models/user.js
+++ models/user.js
@@ -1,30 +1,26 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  posts: {
-    type: [String],
-    default: [],
-  },
-  img: {
-    type: String,
-  },
-  bookmark: {
-    type: [String],
-    default: [],
-  },
-  createdAt: {
-    type: Date,
-    default: new Date(),
-  },
+	name: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	posts: {
+		type: [String],
+		default: [],
+	},
+	img: {
+		type: String,
+	},
+	createdAt: {
+		type: Date,
+		default: new Date(),
+	},
 });
 
 module.exports = mongoose.models.User || mongoose.model("User", userSchema);
